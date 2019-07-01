e3c4d750d2a21e91da125f2f0c4e3ba07fcecbb0
Lukas Turcani <lukasturcani93@gmail.com>
2019-07-01 23:36:25 +0100

    Removed tex docs.

--- docs/conf.py
+++ docs/conf.py
@@ -13,29 +13,9 @@ templates_path = []
 html_static_path = []
 htmlhelp_basename = 'codestyledoc'
 latex_elements = {}
-latex_documents = (
-    master_doc,
-    'code_style.tex',
-    'code_style Documentation',
-    'Lukas Turcani',
-    'manual'
-)
-man_pages = [(
-    master_doc,
-    'code_style',
-    'code_style Documentation',
-    [author],
-    1
-)]
-texinfo_documents = [(
-    master_doc,
-    'code_style',
-    'code_style Documentation',
-    author,
-    'code_style',
-    'Code style guidelines.',
-    'Miscellaneous'
-)]
+latex_documents = ()
+man_pages = []
+texinfo_documents = []
 
 html_theme_options = {
     'collapse_navigation': False
